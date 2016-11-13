99b5fcfe2df92816af1eb0d1473acb98e7632dbc
idlemindss <kristinavan23@yahoo.com>
2016-11-13 16:38:52 -0600

    redesign of main, animatable elements
    fade in-out effect on services and qualities section, restyled
    qualities section, different font on main

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
 'use strict';
 
 $(document).ready(function(){
-
-  $('.title').addClass('animated flipInX');
+  $('.title, .emailRedirect').delay(800).addClass('animated fadeIn').fadeIn(900);
 
 });
+
+var $animationElements = $('.animationElements');
+var $window = $(window);
+
+function check_if_in_view() {
+  var windowHeight = $window.height();
+  var windowTopPosition = $window.scrollTop();
+  var windowBottomPosition = (windowTopPosition + windowHeight);
+
+  $.each($animationElements, function() {
+    var $element = $(this);
+    var elementHeight = $element.outerHeight();
+    var elementTopPosition = $element.offset().top;
+    var elementBottomPosition = (elementTopPosition + elementHeight);
+
+
+  // check to see if this current container is within viewport
+    if ((elementBottomPosition >= windowTopPosition) &&
+      (elementTopPosition <= windowBottomPosition)) {
+      $element.addClass('in-view');
+
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+};
+
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
